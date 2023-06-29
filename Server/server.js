a7ba1b06c336a38f2809363981a9114d6ef1a2d1
HamZik <b1y9mn@gmail.com>
2023-06-29 16:57:51 +0900

    nodemon 오류 발생

--- Server/server.js
+++ Server/server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+import { createProxyMiddleware } from 'http-proxy-middleware';
 
 app.listen(8080, () => {
   console.log('listening on 8080');
@@ -15,3 +16,13 @@ app.get('/', (req, res) => {
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../Client/dist/index.html'));
 })
+
+module.exports = function(app) {
+  app.use(
+    '/api',
+    createProxyMiddleware({
+      target: 'http://localhost:8080',
+      changeOrigin: true,
+    })
+  );
+};
