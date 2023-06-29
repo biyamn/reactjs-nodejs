a528e57e5f40b0bbd2a429837f7ed24fc84e540b
HamZik <b1y9mn@gmail.com>
2023-06-29 17:11:12 +0900

    chore: 오류 부분 삭제

--- Server/server.js
+++ Server/server.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const app = express()
 const path = require('path')
-import { createProxyMiddleware } from 'http-proxy-middleware';
 
 app.listen(8080, () => {
   console.log('listening on 8080');
@@ -16,13 +15,3 @@ app.get('/', (req, res) => {
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../Client/dist/index.html'));
 })
-
-module.exports = function(app) {
-  app.use(
-    '/api',
-    createProxyMiddleware({
-      target: 'http://localhost:8080',
-      changeOrigin: true,
-    })
-  );
-};
